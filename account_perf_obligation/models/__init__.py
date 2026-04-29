daba1e3555f4301a3a303928b49f6a4e1536c834
Benoit Aimont <benoit.aimont@acsone.eu>
2026-04-29 14:10:27 +0200

    [ADD] account_perf_obligation - add schedule table on obligation form view

--- account_perf_obligation/models/__init__.py
+++ account_perf_obligation/models/__init__.py
@@ -1,4 +1,6 @@
-from . import perf_obligation
 from . import account_move_line
+from . import perf_obligation
+from . import perf_obligation_schedule_expense
+from . import perf_obligation_schedule_income
 from . import res_company
 from . import res_config_settings
